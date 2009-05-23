ef81106110abcd03cbb16d385e644137e9126b56
Joel Hayhurst <joelmichael@gmail.com>
2009-05-22 19:26:29 -0700

    404 if nothing routes

--- lib/routes.c
+++ lib/routes.c
@@ -1,6 +1,10 @@
+#include "routes.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "lib/response.h"
 #include "controllers/user_controller.h"
 
 void route(const char* host, const char* uri, const char* method,
@@ -17,5 +21,10 @@ void route(const char* host, const char* uri, const char* method,
   }
   else if(strstr(uri, "/users/delete/") == uri) {
     //delete_user(atoi(uri+14));
+  }
+  else {
+    set_response_code(404);
+    print_headers();
+    printf("404 Not Found\n");
   }
 }
