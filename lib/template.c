260d1609c2fb0ab8de07b6307da58917071a4b5d
Joel Hayhurst <joelmichael@gmail.com>
2009-05-28 17:00:25 -0700

    new tnode system first pass

--- lib/template.c
+++ lib/template.c
@@ -5,12 +5,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-// could feasibly pre-calculate file sizes and store on stack
+// this loads the template up into a data structure for fast substitution
+// mallocs a bunch of stuff but we don't have to free it because it's permanent until program exit
+// basically a linked list of nodes containing either text or a variable for substitution
 
 void cache_template(struct tcache* tc, const char* filename) {
   FILE* file;
   struct stat st;
   char path[42];
+  char line[LINE_MAX];
+  int length;
+  struct tnode* last = tc->first;
   
   strcpy(path, "../templates/");
   strcat(path, filename);
@@ -18,73 +23,120 @@ void cache_template(struct tcache* tc, const char* filename) {
   
   stat(path, &st);
   tc->length = st.st_size;
-  tc->content = (char*)malloc(tc->length + 1);
     
   file = fopen(path, "r");
-  fread(tc->content, tc->length, 1, file);
-  tc->content[tc->length] = '\0';
+
+  while(!feof(file)) {
+    fgets(line, LINE_MAX, file);
+    parse_line(tc, line);
+  }
+
   fclose(file);
 }
 
-void define_tvar(struct tvar* tv, const char* name, const char* value) {
-  strcpy(tv->name, name);
-  tv->name_length = strlen(tv->name);
-  tv->value = (char*)value;
-  tv->value_length = strlen(value);
-}
 
-char* parse_template(const struct tcache tc, const struct tvar* tvars, const int tvar_count) {
-  unsigned int length;
-  char* parsed;
-  int i;
-  char* ptr;
-  char* lastptr;
-  char* parsedptr;
-  unsigned int dist;
-  
-  length = tc.length;
-  
-  for(i = 0; i < tvar_count; i++) {
-    length -= tvars[i].name_length + 4;
-    length += tvars[i].value_length;
-  }
-  
-  parsed = (char*)malloc(length + 1);
-  
-  parsedptr = parsed;
-  lastptr = tc.content;
-  ptr = tc.content;
+// create tnodes out of the line and return the last one
 
-  while(ptr = strstr(ptr, "{{")) {    
-    // copy everything between ptr and lastptr into parsed
-    dist = ptr - lastptr;
-    strncpy(parsedptr, lastptr, dist);
-    parsedptr += dist;
-    lastptr = ptr;
+static void parse_line(struct tcache* tc, const char* line) {
+  char* ptr = line;
+  char* lastptr = line;
+  char* startptr, endptr;
+  struct tnode* tn = tc->last;
+  int textlen, varlen;
+  int linelen = strlen(line);
+  
+  while(ptr = strstr(ptr, "{{")) {
+    startptr = ptr + 2;
+    endptr = strstr(startptr, "}}");
     
-    for(i = 0; i < tvar_count; i++) {
-      if(strncmp(ptr+2, tvars[i].name, tvars[i].name_length) == 0 &&
-         strncmp(ptr+2+tvars[i].name_length, "}}", 2) == 0)
-      {
-        // if there's a match, copy the replacement and update pointers
-        strncpy(parsedptr, tvars[i].value, tvars[i].value_length);
-        parsedptr += tvars[i].value_length;
-        ptr += 2 + tvars[i].name_length + 2;
-        break;
-      }
-    }
-    
-    // increment one if nothing matched
-    if(lastptr == ptr) {
+    // continue if this does not close
+    if(endptr == NULL) {
       ptr++;
+      continue;
     }
-    else {
+    
+    varlen = endptr - startptr;
+    
+    // check for a valid variable name
+    if(strspn(startptr, VARNAME_CHARS) == varlen) {
+      textlen = ptr - lastptr;
+      
+      // create a text tnode of everything up to this point if there is anything
+      if(textlen > 0) {
+        add_text_node(tc, lastptr, textlen);
+      }
+            
+      // create a var tnode
+      add_var_node(tc, startptr, varlen);
+      
+      ptr = endptr+2;
       lastptr = ptr;
     }
+    else {
+      // continue if not
+      ptr++;
+    }
   }
   
-  // one last copy
-  strcpy(parsedptr, lastptr);
+  // create a text tnode of whatever remains
+  add_text_node(tc, lastptr, linelen - (lastptr - line));
+}
+
+
+static void add_text_tnode(struct tcache* tc, const char* text, const int textlen) {
+  struct tnode* tn;
+  char* oldtext;
+  int oldtextlen;
   
-  return parsed;
+  // coalesce the previous text node if possible
+  if(tc->last != NULL && tc->last.text != NULL) {
+    tn = tc.last;
+    oldtext = tn->text;
+    oldtextlen = strlen(oldtext);
+    tn->text = (char*)malloc(oldtextlen + textlen + 1);
+    strncpy(tn->text, text, oldtextlen + textlen);
+    tn->text[oldtextlen + textlen] = '\0'];
+  }
+  else {
+    tn = (struct tnode*)malloc(sizeof(struct tnode));
+    tn->text = (char*)malloc(textlen+1);
+    strncpy(tn->text, text, textlen);
+    tn->text[textlen] = '\0';
+  }
+  
+  add_node(tn);
+}
+
+static void add_var_tnode(struct tcache* tc, const char* varname, const int varlen) {
+  struct tnode* tn;
+  
+  tn = (struct tnode*)malloc(sizeof(struct tnode));
+  tn->varname = (char*)malloc(varlen+1);
+  strncpy(tn->varname, varname, varlen);
+  tn->varname[varlen] = '\0';
+  
+  add_node(tn);
+}
+
+static void add_tnode(struct tcache* tc, struct tnode* tn) {
+  if(tc->first == NULL) {
+    tc->first = tn;
+  }
+  else {
+    tc->last->next = tn;
+  }
+  tc->last = tn;
+}
+
+void define_tsub(struct tsub* ts, const char* name, char* value) {
+  strcpy(ts->name, name);
+  ts->value = value;
+  ts->value_length = strlen(value);
+}
+
+// this does all the substitutions on the tcache and returns a malloced string
+// which the user will have to free manually
+
+char* perform_subs(struct tcache* tc, struct tsub* tsubs) {
+  
 }
--- lib/template.h
+++ lib/template.h
@@ -1,15 +1,23 @@
 #ifndef template_h
 #define template_h
 
+#define LINE_MAX 512
+#define VARNAME_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+
 struct tcache {
   char filename[24];
-  unsigned int length;
-  char* content;
+  struct tnode* first = NULL;
+  struct tnode* last = NULL;
+};
+
+struct tnode {
+  struct tnode* next = NULL;
+  char* text = NULL;
+  char* varname = NULL;
 };
 
 struct tvar {
-  char name[16];
-  int name_length;
+  char[16] name;
   char* value;
   unsigned int value_length;
 };
@@ -18,6 +26,6 @@ void cache_template(struct tcache* tc, const char* filename);
 
 void define_tvar(struct tvar* tv, const char* name, const char* value);
 
-char* parse_template(const struct tcache tc, const struct tvar* tvars, const int tvar_count);
+char* perform_subs(struct tcache* tc, struct tvar* tvars);
 
 #endif
