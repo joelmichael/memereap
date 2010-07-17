39eac179e7e49c4d2704b03fb2d1a221b872505d
Joel Hayhurst <joelmichael@gmail.com>
2010-07-16 23:21:12 -0700

    route test works great!

--- tests/routes_test.c
+++ tests/routes_test.c
@@ -6,18 +6,35 @@
 #include "lib/request.h"
 #include "lib/response.h"
 
-void example_action() {
-}
+static void example_action1() {}
+static void example_action2() {}
+static void example_action3() {}
 
 void test_add_route() {
-  add_route("/example", example_action);
-  add_route("/example/:id", example_action);
-  add_route("/foo/:name/:dog/hey", example_action);
+  add_route("/example", example_action1);  
+  add_route("/example/:id", example_action2);
+  add_route("/foo/:name/:dog/hey", example_action3);
+}
+
+void test_determine_route() {
+  struct route* route;
+  
+  route = determine_route("/example");
+  assert(route->action == example_action1);
+  
+  route = determine_route("/example/2");
+  assert(route->action == example_action2);
+  
+  route = determine_route("/foo/what/urjfjf/f/f/f/hey");
+  assert(route->action == example_action3);
+  
+  route = determine_route("/nope");
+  assert(route == NULL);
 }
 
 int main(int argc, char** argv) {
   test_add_route();
-  //test_determine_route();
+  test_determine_route();
   
   return EXIT_SUCCESS;
 }
